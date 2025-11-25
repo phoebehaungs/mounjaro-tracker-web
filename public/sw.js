57a124d5790f9bb1feda55d6466fd28c4bd512b4
phoebehaungs <phoebehaung@gmail.com>
2025-11-25 11:30:16 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,13 @@
+// public/sw.js
+self.addEventListener('install', (event) => {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+  // 這裡只是為了滿足 PWA 條件，暫時不攔截請求
+});
